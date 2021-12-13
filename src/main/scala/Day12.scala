6d3538b2a8718f5160215f1f5fe796dd5f44ca81
GoateSo <59705775+GoateSo@users.noreply.github.com>
2021-12-13 10:24:20 -0600

    refactor to use 1 dfs implementation

--- src/main/scala/Day12.scala
+++ src/main/scala/Day12.scala
@@ -1,39 +1,34 @@
 import scala.collection.mutable.HashMap
 import scala.collection.mutable.ArrayBuffer
 import scala.collection.mutable.HashSet
+
 class Day12(lines : IndexedSeq[String]):
-  val r = "([a-zA-z]+)-([a-zA-z]+)".r
+  val alph = "[a-zA-z]"
+  val r = s"($alph+)-($alph+)".r
 
   val adjList = HashMap[String,ArrayBuffer[String]]()
-  for r(a,b) <- lines do
+  for Array(a,b) <- lines.map(_.split("-")) do
     adjList.getOrElseUpdate(a,ArrayBuffer[String]()) += b
     adjList.getOrElseUpdate(b,ArrayBuffer[String]()) += a
-
-  def dfs1(v : String, visited : HashSet[String]) : Int = if v == "end" then 1
-    else 
-      if v(0).isLower then visited += v
-      var n = 0
-      for w <- adjList(v) if !visited(w) do 
-        n += dfs1(w,visited)
-      visited -= v
-      n
   
-  def dfs2(v : String, visited : HashMap[String, Int], used : Boolean) : Int = if v == "end" then 1
+  def dfs(v : String, visited : HashMap[String, Int], used : Boolean) : Int = 
+    if (v == "end") 1
     else
-      if v(0).isLower then visited(v) += 1
+      if (v(0).isLower) visited(v) += 1
       var n = 0
       for w <- adjList(v) do
         val op = visited(w) == 1 && !used
         if visited(w) < 1 || op then
-          n += dfs2(w,visited, used || op)
+          n += dfs(w,visited, used || op)
       visited(v) -= 1
       n
 
-  def run = dfs1("start",HashSet())
-  def run2 = 
+  def mkMap = 
     val map = HashMap[String,Int]()
     for (node,_) <- adjList do
       map(node) = 0
     map("start") = 2
-    dfs2("start", map, false)
+    map
 
+  def run = dfs("start",mkMap, true)
+  def run2 = dfs("start", mkMap, false)
